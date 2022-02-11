f1710801a77cf4d4a3b99351c4af9c0cfa869337
hackett_k <45909680+hackettk@users.noreply.github.com>
2022-02-11 15:50:17 +0800

    sw.js upload

--- a2hs/sw.js
+++ a2hs/sw.js
@@ -1,14 +1,10 @@
 self.addEventListener('install', (e) => {
     e.waitUntil(
         caches.open('fox-store').then((cache) => cache.addAll([
-            '/pwa-examples/a2hs/',
-            '/pwa-examples/a2hs/index.html',
-            '/pwa-examples/a2hs/index.js',
-            '/pwa-examples/a2hs/style.css',
-            '/pwa-examples/a2hs/images/fox1.jpg',
-            '/pwa-examples/a2hs/images/fox2.jpg',
-            '/pwa-examples/a2hs/images/fox3.jpg',
-            '/pwa-examples/a2hs/images/fox4.jpg',
+            '/pwa-example/a2hs/',
+            '/pwa-example/a2hs/index.html',
+            '/pwa-example/a2hs/index.js',
+            '/pwa-example/a2hs/style.css'
         ])),
     );
 });
